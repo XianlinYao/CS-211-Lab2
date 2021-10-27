4dbf6aa2acb6837dfe1ed20795e2094aa0b112cc
XianlinYao <55677914+XianlinYao@users.noreply.github.com>
2021-10-27 03:15:40 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -112,6 +112,41 @@ int mydgetrf(double *A, int *ipiv, int n)
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     /* add your code here */
+    double *y = (double*) malloc(n * sizeof(double));
+    int i, j;
+    double sum;
+    
+    switch(UPLO)
+    {
+        case 'L':
+            y[0] = B[ipiv[0]];
+            for (i = 1; i < n; i ++)
+            {
+                sum = 0.0;
+                for (j = 0; j < i; j ++)
+                {
+                    sum += y[j] * A[i * n + j];
+                }
+                y[i] = B[ipiv[i]] - sum;
+            }
+            break;
+        case 'U':
+            y[n - 1] = B[n - 1] / A[(n - 1) * n + n - 1];
+            for (i = n - 2; i >= 0; i --)
+            {
+                for (j = n - 1; j > i; j --)
+                {
+                    sum += y[j] * A[i * n + j];
+                }
+                y[i] = (B[i] - sum) / A[i * n + i];
+            }
+            break;
+        default:
+            printf("\nCheck your UPLO input, it must be L or U.\n");
+            break;
+    }
+    
+    memcpy(B, y, sizeof(double) * n);
     return;
 }
 
@@ -157,6 +192,87 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b) 
 {
+    int ib;
+    
+    for (ib = 0; ib < n; ib += b)
+    {
+        end = ib + b - 1;
+        
+        int i, j, k;
+        double *temprow = (double*) malloc(sizeof(double) * b);
+
+        for (i = ib; i < (n - 1); i ++)
+        {
+            int jp = i;
+            pivot = fabs(A[i * n + i]);
+
+            // find pivoting
+            for (j = i + 1; j < n; j ++)
+            {
+                if (fabs(A[j * n + i]) > pivot){
+                    pivot = fabs(A[j * n + i]);
+                    jp = j;
+                }
+            }
+
+            // swap
+            if (pivot == 0)
+            {
+                printf("Warning: Matrix A is singular.")
+                return -1;
+            }
+
+            else
+            {
+                if (jp != i)
+                {
+                    int temp = ipiv[i];
+                    ipiv[i] = ipiv[jp];
+                    ipiv[jp] = temp;
+                    
+                    memcpy(temprow, A + i * n, n * sizeof(double));
+                    memcpy(A + i * n, A + jp * n, n * sizeof(double));
+                    memcpy(A + jp * n, temprow, n * sizeof(double));
+                }    
+            }
+
+            // factorization
+            for (j = i + 1; j < n; j ++)
+                A[j * n + i] = A[j * n + i] / A[i * n + i];
+            for (j = i + 1; j < n; j ++)
+            {
+                for (k = i + 1; k < ib + b; k ++)
+                {
+                    A[j * n + k] = A[j * n + k] - A[j * n + i] * A[i * n + k];
+                }
+            }
+        }
+        free(temprow);
+        
+        // built LL ^ (-1)
+        double *ll = (double*) calloc(sizeof(double) (b * b));
+        
+        for (int i = 0; i < b; i ++)
+        {
+            ll[i * b + i] = 0;
+            for (int j = i + 1; j < b; j ++)
+            {
+                for (k = i; k < j; k ++)
+                {
+                    ll[j * b + i] = ll[j * b + i] - A[ib * n + ib + j * n + k] * ll[k * b + i];
+                }
+            }
+        }
+        
+        // update A(ib:end, end+1:n)
+        for (i = ib; i <= end; i ++)
+        {
+            for (j = end + 1; i < n
+        }
+        
+    }
+    
+    
     return 0;
 }
 
